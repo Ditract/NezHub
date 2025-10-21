3495f86e7e7e835f3de98f778a012718efbdd8d4
hgsanez <hgsanez17@gmail.com>
2025-10-20 21:11:24 -0500

    Initial commit: setup project structure

---
+++ src/main/java/com/nezhub/app/NezHubApplication.java
@@ -0,0 +1,13 @@
+package com.nezhub.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NezHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nezhub/app/NezHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nezhub.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
